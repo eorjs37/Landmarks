ad3c6ad7e22b141154496c0b8f5d530bd5f1f93c
macdaegeon <chleorjs37@gmail.com>
2024-03-09 08:58:54 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 최대건 on 2024/03/09.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
